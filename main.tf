d2ce909ad8bc52127ec5e5cf3ae90ff213b490e2
Tom Richards <twr.devops@gmail.com>
2025-05-20 22:50:52 +0100

    feat: modules directory structure

--- main.tf
+++ main.tf
@@ -13,14 +13,14 @@ module "convertr_api_gateway" {
 module "convertr_lambda" {
   source = "./modules/convertr_lambda"
 
-  iam_role_name         = "convertr_lambda"
-  archive_source        = "convertr_lambda.py"
-  archive_output        = "convertr_lambda_function.zip"
-  function_name         = "convertr_lambda_function"
-  handler               = "index.handler"
-  runtime               = "python3.13"
-  environment_variables = { 
-    foo = "bar" 
+  iam_role_name  = "convertr_lambda"
+  archive_source = "convertr_lambda.py"
+  archive_output = "convertr_lambda_function.zip"
+  function_name  = "convertr_lambda_function"
+  handler        = "index.handler"
+  runtime        = "python3.13"
+  environment_variables = {
+    foo = "bar"
   }
 }
 
