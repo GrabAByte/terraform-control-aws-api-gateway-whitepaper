d52458fbe213840748ae8bc9e70b64c3793f4c61
Tom Richards <30478573+GrabAByte@users.noreply.github.com>
2025-05-21 08:00:09 +0100

    feat: Add terraform destroy and lambda ARN lookup (#9)

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ module "convertr_api_gateway" {
   api_authorization_method = "NONE"
   integration_http_method  = "POST"
   integration_type         = "AWS"
-  lambda_invoke_arn        = "<data.lambda.arn>" # placeholder
+  lambda_invoke_arn        = module.convertr_lambda.lambda_arn
 }
 
 module "convertr_lambda" {
