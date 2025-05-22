727ff24722a9e34ba04b596b05d562e865f78b08
Tom Richards <twr.devops@gmail.com>
2025-05-22 15:31:41 +0100

    fix: subtle uncleaniness in the code

--- modules/convertr_api_gateway/main.tf
+++ modules/convertr_api_gateway/main.tf
@@ -24,7 +24,7 @@ resource "aws_api_gateway_method" "convertr_method" {
 resource "aws_api_gateway_method_response" "convertr_response" {
   rest_api_id = aws_api_gateway_rest_api.convertr_api.id
   resource_id = aws_api_gateway_resource.convertr_path.id
-  http_method = "PUT"
+  http_method = aws_api_gateway_method.convertr_method.http_method
   status_code = 200
   response_models     = {
     "application/json" = "Empty"
@@ -35,7 +35,7 @@ resource "aws_api_gateway_integration" "convertr_integration" {
   rest_api_id             = aws_api_gateway_rest_api.convertr_api.id
   resource_id             = aws_api_gateway_resource.convertr_path.id
   integration_http_method = var.integration_http_method
-  http_method             = "PUT"
+  http_method             = aws_api_gateway_method.convertr_method.http_method
   passthrough_behavior    = var.passthrough_behaviour
   type                    = var.integration_type
   uri                     = var.lambda_invoke_arn
@@ -49,7 +49,7 @@ resource "aws_api_gateway_integration" "convertr_integration" {
 resource "aws_api_gateway_integration_response" "convertr_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.convertr_api.id
   resource_id = aws_api_gateway_resource.convertr_path.id
-  http_method = "PUT"
+  http_method = aws_api_gateway_method.convertr_method.http_method
   status_code = aws_api_gateway_method_response.convertr_response.status_code
 
   # Transforms the backend JSON response to XML
