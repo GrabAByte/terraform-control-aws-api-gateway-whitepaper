24018da4c574f3a2bec48d19169a293d6402e7bf
Tom Richards <twr.devops@gmail.com>
2025-05-23 17:22:00 +0100

    fix: tidy up and variablize code

--- backends/production.tf
+++ backends/production.tf
@@ -1,3 +1,10 @@
 terraform {
   required_version = ">= 1.11"
+
+  backend "s3" {
+    bucket  = "grababyte-tfstate"
+    key     = "convertr/convertr.tfstate"
+    region  = "eu-west-2"
+    encrypt = true
+  }
 }
