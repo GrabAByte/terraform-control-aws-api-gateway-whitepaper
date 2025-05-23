4e2cf2245affda2c491b21b33c74ec1c46ac5e71
Tom Richards <twr.devops@gmail.com>
2025-05-23 17:32:43 +0100

    fix: tidy up and variablize code

--- backends/production.tf
+++ backends/production.tf
@@ -1,8 +1,4 @@
-terraform {
-  backend "s3" {
-    bucket  = "grababyte-tfstate"
-    key     = "convertr/convertr.tfstate"
-    region  = "eu-west-2"
-    encrypt = true
-  }
-}
+bucket  = "grababyte-tfstate"
+key     = "convertr/convertr.tfstate"
+region  = "eu-west-2"
+encrypt = true
