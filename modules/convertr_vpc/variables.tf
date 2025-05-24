66cb34fae8151735c784e15d48fd90ab10795999
Tom Richards <twr.devops@gmail.com>
2025-05-24 16:50:10 +0100

    feat: vpc configuration

--- modules/convertr_vpc/variables.tf
+++ modules/convertr_vpc/variables.tf
@@ -13,7 +13,7 @@ variable "vpc_cidr_block" {
 variable "subnet_cidr_block" {
   type        = string
   description = "The CIDR block range to allocate to the VPC"
-  default     = "10.0.0.0/16"
+  default     = "10.0.1.0/24"
 }
 
 variable "vpc_endpoint_type" {
