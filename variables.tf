fb20813ae0180c27ed4f6b2358ca849c03560083
Tom Richards <twr.devops@gmail.com>
2025-05-24 14:35:55 +0100

    feat: vpc configuration

--- variables.tf
+++ variables.tf
@@ -1,4 +1,23 @@
+variable "environment_map" {
+  type        = map(any)
+  description = "The environment lookup for each workspace"
+  default = {
+    development    = "development"
+    pre-production = "pre-production"
+    production     = "production"
+  }
+}
+
 variable "region" {
-  type    = string
-  default = "eu-west-2"
+  type        = string
+  description = "The AWS region in which to deploy"
+  default     = "eu-west-2"
+}
+
+locals {
+  environment = lookup(var.environment_map, terraform.workspace, "development")
+  tags = {
+    environment = local.environment
+    project     = "convertr-demo"
+  }
 }
