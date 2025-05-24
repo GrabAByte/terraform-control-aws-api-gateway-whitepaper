6968c599d041427d277ff303cbc09775cc5fdb0c
Tom Richards <twr.devops@gmail.com>
2025-05-24 19:44:03 +0100

    working

--- test/api-call.sh
+++ test/api-call.sh
@@ -2,14 +2,20 @@
 
 # example usage: bash -x api-call.sh https://<API_ID>.execute-api.eu-west-2.amazonaws.com/v1beta1/upload tom-richards.jpg
 
+usage () {
+  echo "Usage: bash -x api-call.sh   https://<API_ID>.execute-api.<REGION>.amazonaws.com/<STAGE>/<PATH>  tom-richards.jpg"
+}
+
 if [ -z ${1+x} ] && [ -z ${2+x} ]; then
-  echo "Arguments are required to run scripting, exiting"
+  echo "Arguments are required to run scrip, exiting"
+  usage
   exit 1
 else
   API_URL="$1"
   IMAGE_FILE="$2"
 fi
 
+## hardcoded for purposes of demonstrating lambda authorization
 BEARER_TOKEN=$(cat < ~/.bearer | base64 -d)
 
 curl -X POST \
