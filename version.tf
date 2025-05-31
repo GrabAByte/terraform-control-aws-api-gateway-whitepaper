3625a968b621e89585a9fcce739b59f5be9d7e7a
Tom Richards <twr.devops@gmail.com>
2025-05-31 12:05:22 +0100

    fix: tfstate bucket implementation

--- version.tf
+++ version.tf
@@ -11,8 +11,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "grababyte-tfstate"
-    key     = "whitepapers/development/api-lambda-trigger-to-s3.tfstate"
+    bucket  = "api-lambda-trigger-to-s3"
+    key     = "tfstate"
     region  = "eu-west-2"
     encrypt = true
   }
