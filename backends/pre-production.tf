684e3403789849c5e50b109fccb8355c786f0f95
Tom Richards <twr.devops@gmail.com>
2025-05-31 12:02:30 +0100

    fix: tfstate bucket implementation

--- backends/pre-production.tf
+++ backends/pre-production.tf
@@ -1,4 +1,4 @@
-bucket  = "grababyte-tfstate"
-key     = "whitepapers/pre-production/api-lambda-trigger-to-s3.tfstate"
+bucket  = "grababyte-api-lambda-trigger-to-s3"
+key     = "tfstate"
 region  = "eu-west-2"
 encrypt = true
