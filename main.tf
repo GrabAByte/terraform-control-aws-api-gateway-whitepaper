2734ba84d88360304e51e4fd940c26ecf51f40f3
Tom Richards <twr.devops@gmail.com>
2025-08-07 20:31:28 +0100

    updates

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ module "vpc" {
   source = "github.com/GrabAByte/terraform-module-aws-vpc?ref=v1.0.0"
 }
 
+module "s3_auth" {
+  source          = "github.com/GrabAByte/terraform-module-aws-s3?ref=v1.0.2"
+  bucket_name     = "grababyte-auth-bucket"
+  log_bucket_name = "grababyte-auth-log-bucket"
+
+  tags = local.tags
+}
+
 module "s3_upload" {
   source          = "github.com/GrabAByte/terraform-module-aws-s3?ref=v1.0.2"
   bucket_name     = "grababyte-upload-bucket"
@@ -18,19 +26,32 @@ module "s3_download" {
   tags = local.tags
 }
 
-module "lambda_upload" {
-  source = "github.com/GrabAByte/terraform-module-aws-lambda?ref=v1.0.1"
+module "lambda_auth" {
+  source = "github.com/GrabAByte/terraform-module-aws-lambda?ref=feat/extend"
 
-  auth_function_name   = "auth_lambda"
-  auth_handler         = "auth_function.lambda_handler"
-  auth_runtime         = "python3.13"
-  auth_lambda_source   = "auth_function.py"
-  auth_lambda_filename = "auth_function.zip"
-  function_name        = "image_uploader"
-  handler              = "lambda_function.lambda_handler"
-  iam_role_name        = "lambda_exec_role"
-  lambda_source        = "lambda_function.py"
-  runtime              = "python3.13"
+  function_name = "auth_lambda"
+  handler       = "auth_function.lambda_handler"
+  iam_role_name = "lambda_exec_role"
+  runtime       = "python3.13"
+  lambda_source = "auth_function.py"
+
+  bucket_name     = module.s3_auth.bucket_name
+  bucket_arn      = module.s3_auth.bucket_arn
+  vpc_subnet_0    = module.vpc.vpc_subnet_0
+  vpc_subnet_1    = module.vpc.vpc_subnet_1
+  security_groups = module.vpc.security_groups
+
+  tags = local.tags
+}
+
+module "lambda_upload" {
+  source = "github.com/GrabAByte/terraform-module-aws-lambda?ref=feat/extend"
+
+  function_name = "image_uploader"
+  handler       = "lambda_function.lambda_handler"
+  iam_role_name = "lambda_exec_role"
+  lambda_source = "lambda_function.py"
+  runtime       = "python3.13"
 
   bucket_name     = module.s3_upload.bucket_name
   bucket_arn      = module.s3_upload.bucket_arn
@@ -42,18 +63,13 @@ module "lambda_upload" {
 }
 
 module "lambda-download" {
-  source = "github.com/GrabAByte/terraform-module-aws-lambda?ref=v1.0.1"
+  source = "github.com/GrabAByte/terraform-module-aws-lambda?ref=feat/extend"
 
-  auth_function_name   = "auth_lambda"
-  auth_handler         = "auth_function.lambda_handler"
-  auth_runtime         = "python3.13"
-  auth_lambda_source   = "auth_function.py"
-  auth_lambda_filename = "auth_function.zip"
-  function_name        = "image_downloader"
-  handler              = "lambda_function.lambda_handler"
-  iam_role_name        = "lambda_exec_role"
-  lambda_source        = "lambda_function.py"
-  runtime              = "python3.13"
+  function_name = "image_downloader"
+  handler       = "lambda_function.lambda_handler"
+  iam_role_name = "lambda_exec_role"
+  lambda_source = "lambda_function.py"
+  runtime       = "python3.13"
 
   bucket_name     = module.s3_download.bucket_name
   bucket_arn      = module.s3_download.bucket_arn
@@ -67,7 +83,9 @@ module "lambda-download" {
 module "api_gateway" {
   source = "github.com/GrabAByte/terraform-module-aws-api-gateway?ref=feat/extend"
 
-  api_name = "image"
+  api_name               = "image"
+  lambda_auth_invoke_arn = module.lambda_upload.auth_invoke_arn
+
   #api_routes = {
   #  "upload" = {
   #    api_path_part = "upload"
@@ -77,7 +95,6 @@ module "api_gateway" {
   #      "image/jpeg",
   #      "image/png"
   #    ]
-  #    lambda_auth_invoke_arn = module.lambda_upload.auth_invoke_arn
   #    lambda_invoke_arn      = module.lambda_upload.invoke_arn
   #    lambda_name            = module.lambda_upload.name
   #  },
